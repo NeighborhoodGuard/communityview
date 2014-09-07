099574ff2883ecb10a4d4e10c0f7166c3c817655
Douglas Kerr <dougk@halekerr.com>
2014-09-07 11:37:47 -0700

    Improve stats.proc_stats() unit testing.
    testStats.py:
    - Parameterize proc_stats() test and verification.
    - Add MockTime class to manipulate time.time() to improve processing-
    latency testing.
    
    testutils.py:
    - Add filename_to_time() and dirname_to_datetime().

--- CommunityView/src/test/testStats.py
+++ CommunityView/src/test/testStats.py
@@ -9,6 +9,39 @@ import testsettings
 import os
 import datetime
 import time
+from testutils import filename_to_time, dirname_to_datetime
+
+class MockTime():
+    """Monkey patch time.time() to return a timestamp set by set_time()."""
+    
+    orig_time = None
+    fake_timestamp = None
+        
+    @classmethod
+    def _fake_time(cls):
+        return cls.fake_timestamp
+            
+    @classmethod        
+    def set_time(cls, timestamp):
+        """Patch time.time() to return the specified timestamp."""
+        if cls.orig_time == None:
+            cls.orig_time = time.time
+            time.time = cls._fake_time        
+        cls.fake_timestamp = timestamp
+        
+    @classmethod
+    def restore_time(cls):
+        """Undo the patch and restore time.time() to its original 
+        functionality."""
+        if cls.orig_time != None:
+            time.time = cls.orig_time
+            cls.orig_time = None
+        cls.fake_timestamp = None
+        
+    @classmethod
+    def real_time(cls):
+        """Return the value of the unpatched time.time() function."""
+        return cls.orig_time()
 
 
 class TestStats(unittest.TestCase):
@@ -22,15 +55,55 @@ class TestStats(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def call_proc_stats(self, datecam, fnprefix, nfiles, uplat, proclat):
+        """Call proc_stats() nfiles times with the given datecam and file name 
+        prefix (e.g., "12-01-00"), also passing it an appropriate mtime and 
+        manipulating the return value of time.time() to represent the specified
+        upload latency and processing latency in minutes."""
+        date_dt = dirname_to_datetime(datecam[0])
+        for i in range(nfiles):
+            fn = fnprefix + "-%05d.jpg" % (i+1)
+            mtime = time.mktime((date_dt + filename_to_time(fn)).timetuple()) \
+                    + uplat * 60
+            MockTime.set_time(float(mtime + proclat * 60))
+            stats.proc_stats(datecam, fn, mtime)
+            MockTime.restore_time()
+    
     def test000proc_stats(self):
         datecam = ("2014-07-01", "cam1")
-        mt = time.mktime(datetime.datetime(2014, 07, 01, 00, 5, 00).timetuple())
-        stats.proc_stats(datecam, "00-01-00-12345.jpg", mt)
+        fnprefix = "00-01-00"
+        nfiles = 5
+        uplat = 24*60+4
+        proclat = 10
+        self.call_proc_stats(datecam, fnprefix, nfiles, uplat, proclat)
+
+        keys = stats.statdict.keys()
+        for k in keys:
+            print str(k)
+            table = stats.statdict[k][stats.TABLE]
+            for i in range(7):
+                print table[i]
+            print
         
-        print stats.statdict.keys()
-        table = stats.statdict[datecam][stats.TABLE]
-        for i in range(7):
-            print table[i]
+        # upload latency statistics
+        uplat_table = stats.statdict[datecam][stats.TABLE]
+        uplat_row = filename_to_time(fnprefix).seconds/60
+        assert uplat_table[uplat_row][stats.NUPLOAD] == nfiles, "%d, %d" \
+                % (uplat_table[uplat_row][stats.NUPLOAD], nfiles)
+        assert uplat_table[uplat_row][stats.AVGUPLAT] == uplat, "%d, %d" \
+                % (uplat_table[uplat_row][stats.AVGUPLAT], uplat)
+                    
+        # processing latency statistics
+        uplat_td = datetime.timedelta(minutes=uplat)
+        upload_date = (dirname_to_datetime(datecam[0]) + uplat_td) \
+                         .strftime("%Y-%m-%d")
+        proclat_table = stats.statdict[(upload_date, datecam[1])][stats.TABLE]
+        proclat_row = uplat_row + uplat_td.seconds/60   # strip uplat days
+        assert proclat_table[proclat_row][stats.NPROC] == nfiles, "%d, %d" \
+                % (proclat_table[proclat_row][stats.NPROC], nfiles)
+        assert proclat_table[proclat_row][stats.AVGPROCLAT] == proclat,"%d, %d"\
+                % (proclat_table[proclat_row][stats.AVGPROCLAT], proclat)
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testStats']
