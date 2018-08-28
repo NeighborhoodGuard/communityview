821b7a37b54087fd288cab45f5d67d744006663b
Douglas Kerr <dougk@halekerr.com>
2018-08-27 17:32:32 -0700

    Add first cut install script for bare Ubuntu server
    - implementation not complete; just a snapshot
    - make communityview a service

---
+++ CommunityView/confcvserver/confcvserver.sh
@@ -0,0 +1,287 @@
+#!/bin/sh
+
+################################################################################
+#
+# Copyright (C) 2018 Neighborhood Guard, Inc.  All rights reserved.
+# Original author: Douglas Kerr
+# 
+# This file is part of FTP_Upload.
+# 
+# FTP_Upload is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# FTP_Upload is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with FTP_Upload.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+ 
+# This script will collect configuration values from the user, then install
+# and configure all software required to turn this machine into a Neighborhood
+# Guard upload machine.  It will receive images from properly configured IP
+# cameras and upload them to a cloud server running Neighborhood Guard's
+# CommunityView software.
+
+# version of the confcvserver software
+version="1.0.0"
+
+. ./utils.sh
+. ./confui.sh
+
+confile=cvserver.conf
+
+# The incoming directory that the cameras or upload machine will be
+# uploading to.  Currently, this is the same as the top directory
+# for the Web server.  One day, CommunityView will be changed so that
+# this is not the case.
+inc_dir=/var/www/html
+
+code_dir=/opt/communityview
+config_dir=/etc/opt/communityview # XXX future use
+var_dir=/var/opt/communityview
+log_dir=$var_dir/log
+systemd_dir=/lib/systemd/system
+
+# log file for this script
+scriptlog=confcvserver.log
+
+# shell to use as a no-login shell for the upload machines's FTP account
+nologinshell=/bin/false
+
+# global to hold the name of the section that produced an unexpected error
+#
+task=""
+
+# install the package or packages indicated in the first argument
+install() {
+    local pkgs="$1"
+    local wtime=5
+    local maxtries=36
+    local tries=$maxtries
+    while ! apt-get -qqy install $pkgs
+    do
+        if [ $tries -eq $maxtries ]
+        then
+            echo "(First install attempt failed." \
+                "Will retry for up to three minutes.)" > /dev/tty
+        fi
+        echo "Waiting $wtime seconds and trying again."
+        sleep "$wtime"
+        tries=`expr $tries - 1`
+        if [ "$tries" = 0 ]
+        then
+            echo "CANNOT INSTALL REQUIRED SYSTEM SOFTWARE"
+            return 1
+        fi
+    done
+    return 0
+}
+
+# on unexpected exit, print an error message and the error output for that task
+#
+errorexit() {
+    tac "$scriptlog" | sed "/$task/,\$d" | tac >&2
+    echo -n "\033[31m\033[1m" > /dev/tty   # red, bold text
+    echo "An unexpected error occurred while $task." | tee -a "$scriptlog" >&2
+    echo "Please see above or examine the log file: $scriptlog." \
+        | tee -a "$scriptlog" >&2
+    echo -n "\033[0m" > /dev/tty    # reset text style
+    echo `date --rfc-3339=seconds` "Error exit confcvserver" >> "$scriptlog"
+    exit 1
+}
+
+# make the directory specified, unless it already exists
+#
+mk_dir() {
+    if [ ! -d "$1" ]
+    then
+        mkdir $1
+    fi
+}
+
+# take the config information and build the server
+#
+configure() {
+    local cfg=$conf_file
+    
+    # Set up to catch unexpected errors and notify user
+    #
+    trap errorexit EXIT
+    set -e
+
+    task="updating the available system software listing"
+    echo "***** $task" | tee /dev/tty
+    # update and upgrade the system
+    apt-get update  # info output to log
+    # XXX apt-get upgrade
+
+    task="shutting down any previous CommunityView"
+    echo "***** $task" | tee /dev/tty
+    # stop the service if it's running
+    # and remove the service file if it's there
+    systemctl stop communityview || true
+    systemctl disable communityview || true
+    tgt=$systemd_dir/communityview.service
+    rm -f "$tgt"
+
+    
+    task="installing and configuing Apache Web server"
+    echo "***** $task" | tee /dev/tty
+    install apache2
+    local up_user=`get_config $confile up_user`
+
+    # configure for upload FTP.  It seems that the only simple way to
+    # deny login to the upload user but allow the upload user to connect
+    # via FTP is to put the no-login-shell into /etc/shells then set
+    # the upload user's shell to it.  If it's not in /etc/shells,
+    # proftpd won't allow the user to connect via FTP
+    #
+    task="installing proftpd and configuring FTP access"
+    echo "***** $task" | tee /dev/tty
+
+    # install debconf-utils so we can pre-configure proftpd not to ask the user
+    # whether it should be run under inetd or standalone
+    install debconf-utils  # info output to log
+    echo "proftpd-basic shared/proftpd/inetd_or_standalone select standalone" \
+        | debconf-set-selections   # info output to log
+
+    install proftpd
+        
+    if ! grep "^$nologinshell\$" /etc/shells > /dev/null
+    then
+        echo $nologinshell >> /etc/shells
+    fi
+
+    # create the local FTP user account for the upload machine
+    # and give it access to the incoming images dir
+    #
+    if id -u $up_user > /dev/null 2>&1
+    then
+        deluser --quiet $up_user 
+    fi
+    useradd -d $inc_dir -U -s $nologinshell $up_user
+    chown -R $up_user:$up_user $inc_dir    
+    chmod 775 $inc_dir
+    echo "$up_user:`get_config $confile up_pass`" | chpasswd
+    
+    # limit FTP users to their login directory and below 
+    local cf=/etc/proftpd/proftpd.conf
+    if ! grep -E '^DefaultRoot\s+~' "$cf" > /dev/null
+    then
+        echo \
+           "# The configuration below was added by the confcvserver script" \
+            >> $cf
+        echo 'DefaultRoot ~' >> $cf
+    fi
+    
+    # set proftpd up to be run on boot and restart it with the new config
+    update-rc.d proftpd defaults
+    service proftpd restart
+
+    task="installing python and its imaging library"
+    echo "***** $task" | tee /dev/tty
+    install "python python-imaging"
+
+    task="installing and configuring CommunityView server"
+    echo "***** $task" | tee /dev/tty
+    local our_dir=`dirname $(readlink -e "$0")`
+
+    # make the required dirs
+    mk_dir $code_dir
+    mk_dir $config_dir
+    mk_dir $var_dir
+    mk_dir $log_dir
+    chown $up_user:$up_user $log_dir
+
+    # copy the code
+    cp $our_dir/../src/localsettings.py $code_dir
+    cp $our_dir/../src/baseclasses.py $code_dir
+    cp $our_dir/../src/communityview.py $code_dir
+    
+    # edit the code to set config values :-P
+    #
+    set_config_value $code_dir/localsettings.py root "\"$inc_dir\""
+    local retain_days=`get_config $confile retain_days`
+    set_config_value $code_dir/localsettings.py retain_days "$retain_days"
+    set_config_value $code_dir/localsettings.py logfile_max_days "$retain_days"
+
+    # for each "cameraN" spec in conf file, add a camera def to settings file
+    local cam
+    local cnum=1
+    local camlist=""
+    while true
+    do
+        cam=`get_config $confile "camera$cnum"`
+        if [ -z "$cam" ]
+        then
+            break
+        fi
+        sname=`echo "$cam" | sed 's/ .*//'`
+        lname=`echo "$cam" | sed 's/^[^ ][^ ]*  *//'`
+        camlist="$camlist    camera(\"$sname\", \"$lname\"),\n"
+        cnum=`expr $cnum + 1`
+    done
+    sed --in-place "/^    camera(/,/[^ ]*\]/d" $code_dir/localsettings.py
+    sed --in-place "/^cameras *= *\\[/a\\$camlist    ]" \
+        $code_dir/localsettings.py 
+
+    # install the communityview service file
+    # and start the service
+    #
+    cp $our_dir/../src/communityview.service "$tgt"
+    # set the user that the service will run as
+    ouruser=`get_config $confile up_user`
+    set_config_value $tgt User "$ouruser"
+    chmod 755 $tgt
+    chown root:root $tgt
+    systemctl enable communityview
+    systemctl start communityview
+
+    # Turn off error trap
+    set +e
+    trap - EXIT
+    
+    echo "***** done" | tee /dev/tty
+}
+    
+main() {
+    # verify that we're root
+    #
+    if [ `whoami` != root -a "$UI_TESTING" != 1 ]
+    then
+        echo "$0: You must run this script as root."
+        echo "Try sudo $0"
+        exit 1
+    fi
+    
+    # start the log
+    echo "\n`date --rfc-3339=seconds` Start confcvserver" >> "$scriptlog"
+
+    
+    # Get the config info from the user.
+    # Exit if the user cancels
+    #
+#   if ! get_info
+#   then
+#       echo `date --rfc-3339=seconds` "User cancelled confcvserver" \
+#           >> "$scriptlog"
+#       exit 1
+#   fi
+    
+    # configure this machine
+    configure >> "$scriptlog" 2>&1
+    echo `date --rfc-3339=seconds` "Normal exit confcvserver" >> "$scriptlog"
+}
+    
+
+if [ ! $UNIT_TEST_IN_PROGRESS ]
+then
+    main
+fi
+
